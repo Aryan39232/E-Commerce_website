ce6939748d4f466dc53ea5aa8a575d3d0179a70a
Aryan39232 <20bcs097@iiitdwd.ac.in>
2023-12-07 23:55:45 +0530

    Password rest part completed.

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { registerUser, loginUser, logout } = require('../controllers/user');
+const { registerUser, loginUser, logout, forgotPassword } = require('../controllers/user');
 const router = express.Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/password/forgot').post(forgotPassword);
 router.route('/logout').get(logout);
 module.exports = router;
